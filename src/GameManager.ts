9265d9d7878d2f282d27b3097857656e05a943f4
produy <nguyentrinhduy2003@gmail.com>
2024-05-23 14:03:12 +0700

    (repos): set up main game and game manager

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,28 +1,63 @@
-import { Buff } from "./buff/Buff.js";
-import { State } from "./state/State.js"
-import { StartState } from "./state/StartState.js";
+import { canvas } from "./MainGame";
+import { Buff } from "./buff/Buff";
+import { Land } from "./land/Land";
+import { Bullet } from "./player/Bullet";
+import { Player } from "./player/Player";
+import { State } from "./state/State";
 
 export class GameManager {
-    time: number;
-    score: number;
-    buffs: Buff[];
-    state: State;
+    private buffs: Buff[];
+    private player: Player;
+    private bullets: Bullet[];
+    private lands: Land[];
+    private state : State
     private static instance: GameManager;
     private constructor() {
-        this.reset()
+        this.reset();
     }
 
     reset() {
-        this.time = 0;
-        this.score = 0;
-        this.state = new StartState();
+        this.player = new Player();
+        this.lands = [];
+        this.buffs = [];
+        this.bullets = [];
     }
 
-    getInstance(): GameManager{
+    static getInstance(): GameManager{
         if (!GameManager.instance){
             GameManager.instance = new GameManager();
         }
         return GameManager.instance;
+    }
+
+    process(period: number){
+        this.state.update(period);
+        this.render();
+    }
+    
+    getPlayer(){
+        return this.player;
+    }
+
+    updateMap(){
+        // remove stuffs below the camera
+        while(this.lands.length > 0 && this.lands[0].getPositionY() >= window.innerHeight + 10){
+            this.lands.shift();
+        }
+
+        // add stuffs above the camera
+        
+    }
+    render(){
+        const ctx = canvas!.getContext('2d');
+        if (ctx){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        this.state.render();
+    }
+    transitionTo(state: State){
+        this.state = state;
+        this.state.setContext(this);
     }
 }
 
--- src/MainGame.ts
+++ src/MainGame.ts
@@ -1,7 +1,43 @@
 import { GameManager } from "./GameManager";
+import { Service } from "./gameEngine/Service";
+import { StartState } from "./state/StartState";
+import { State } from "./state/State";
 
-class MainGame {
-    
+export class MainGame {
+    gameManager: GameManager;
+    state: State
+    constructor(){
+        this.gameManager = GameManager.getInstance();
+    }
+    run()
+    {
+        this.gameManager.transitionTo(new StartState());
+        console.log("New Game Created");
+        requestAnimationFrame(loop);
+    }
+    process(period: number){
+        let progress = period/Service.getInstance().getSpeed();
+        this.gameManager.process(progress);
+    }
 }
 
-new MainGame()
+function loop() {
+    const time = window.performance.now();
+    
+    const delta = time - lastTime;
+    newGame.process(delta);
+    lastTime = time;
+    requestAnimationFrame(loop);
+}
+
+export const canvas = document.getElementById('game') as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let newGame = new MainGame()
+let lastTime = window.performance.now();
+
+// run the game
+newGame.run();
+
+
