b619a371cef6004691ca5f4d489d6afc5d6cbe72
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-24 15:22:31 +0700

    (feat): implement propeller buff

--- src/buff/PropellerBuff.ts
+++ src/buff/PropellerBuff.ts
@@ -1,5 +1,7 @@
+import { BUFF_TIME, PLAYER_VECTOR_ON_PROPELLER_BUFF } from "../constants/Buff";
 import { PropellerBuffFlyweight } from "../constants/ResourcePath";
 import { Player } from "../player/Player";
+import { PlayerState } from "../player/PlayerState";
 import { Buff } from "./Buff";
 
 export class PropellerBuff extends Buff{
@@ -7,6 +9,9 @@ export class PropellerBuff extends Buff{
         super(PropellerBuffFlyweight);
     }
     onReceived(player: Player): void {
-        
+        player.setVector([...PLAYER_VECTOR_ON_PROPELLER_BUFF]);
+        player.setAcceleration([0, 0]);
+        player.setState(PlayerState.WearingPropeller);
+        player.setBuffTime(BUFF_TIME);
     }
 }
