4a2e617b0521990be85d1cbd7625a597043e86d4
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-24 11:10:34 +0700

    (feat): add player name and update map method for game manager

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,25 +1,30 @@
 import { canvas } from "./MainGame";
-import { Buff } from "./buff/Buff";
-import { Land } from "./land/Land";
+import { LAND_HEIGHT, LAND_WIDTH } from "./constants/Land";
+import { PLAYER_START_VECTOR } from "./constants/Player";
+import { WINDOW_HEIGHT, WINDOW_WIDTH } from "./constants/WindowBounds";
+import { Service } from "./gameEngine/Service";
+import { DustLand } from "./land/DustLand";
+import { Land, LandType } from "./land/Land";
+import { MovingLand } from "./land/MovingLand";
+import { NormalLand } from "./land/NormalLand";
 import { Bullet } from "./player/Bullet";
 import { Player } from "./player/Player";
 import { State } from "./state/State";
 
 export class GameManager {
-    private buffs: Buff[];
     private player: Player;
     private bullets: Bullet[];
     private lands: Land[];
     private state : State
+    private playerName: string;
     private static instance: GameManager;
     private constructor() {
         this.reset();
     }
 
     reset() {
-        this.player = new Player();
+        this.player = new Player(); 
         this.lands = [];
-        this.buffs = [];
         this.bullets = [];
     }
 
@@ -36,17 +41,62 @@ export class GameManager {
     }
     
     getPlayer(){
-        return this.player;
+        return this.player!;
     }
-
+    getLands(){
+        return this.lands;
+    }
     updateMap(){
+        // move
+        this.lands.forEach(element => {
+            element.move();
+        });
         // remove stuffs below the camera
-        while(this.lands.length > 0 && this.lands[0].getPositionY() >= window.innerHeight + 10){
+        while(this.lands.length > 0 && this.lands[0].getPositionY() >= WINDOW_HEIGHT + 10){
             this.lands.shift();
         }
 
         // add stuffs above the camera
-        
+        let service = Service.getInstance();
+        while(this.lands.length == 0 || this.lands[this.lands.length - 1].getPositionY() >= 50){
+            let previousHeight = WINDOW_HEIGHT;
+            if (this.lands.length > 0){
+                previousHeight = this.lands[this.lands.length - 1].getPositionY();
+            }
+            let randomNum = service.getRandomInt(0, 0);
+            switch (randomNum) {
+                case LandType.NormalLand:
+                {
+                    let newLand = new NormalLand();
+                    newLand.setPosition([
+                        service.getRandomFloat(0, WINDOW_WIDTH - LAND_WIDTH),
+                        service.getRandomFloat(previousHeight - LAND_HEIGHT - 200, previousHeight - LAND_HEIGHT)
+                    ]);
+                    this.lands.push(newLand);
+                    break;
+                }
+                case LandType.MovingLand:
+                {
+                    let newLand = new MovingLand();
+                    newLand.setPosition([
+                        service.getRandomFloat(0, WINDOW_WIDTH - LAND_WIDTH),
+                        service.getRandomFloat(previousHeight - LAND_HEIGHT - 200, previousHeight - LAND_HEIGHT)
+                    ]);
+                    this.lands.push(newLand);
+                    break;
+                }
+                case LandType.DustLand:
+                {
+                    let newLand = new DustLand();
+                    newLand.setPosition([
+                        service.getRandomFloat(0, WINDOW_WIDTH - LAND_WIDTH),
+                        service.getRandomFloat(previousHeight - LAND_HEIGHT - 200, previousHeight - LAND_HEIGHT)
+                    ]);
+                    this.lands.push(newLand);
+                    break;
+                }
+            }
+        }
     }
     render(){
         const ctx = canvas!.getContext('2d');
