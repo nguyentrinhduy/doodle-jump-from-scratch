7efd76b1ce4cec95f78c88f9998cef7fc9d1b870
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-26 19:04:27 +0700

    (model): create class Monster

---
+++ src/game/monster/Monster.ts
@@ -0,0 +1,15 @@
+import { AnimationGameObject } from '../../game-engine/game-objects/AnimationGameObject'
+import { GameObject } from '../../game-engine/game-objects/GameObject'
+import { Player } from '../player/Player'
+import { PlayerState } from '../player/PlayerState'
+
+export abstract class Monster extends AnimationGameObject {
+    protected velocity: [number, number]
+    constructor(Scene: string[]) {
+        super(Scene)
+    }
+    onCollision(player: Player) {
+        player.setState(PlayerState.Lose)
+    }
+    abstract move(deltaTime: number): void
+}
