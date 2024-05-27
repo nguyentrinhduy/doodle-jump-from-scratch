b28a707fb2c2cf95588b53ffc7f7b83456c047e2
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-27 11:26:39 +0700

    (feat): implement keyboard input

--- src/game-engine/input-handler/KeyboardInput.ts
+++ src/game-engine/input-handler/KeyboardInput.ts
@@ -1,1 +1,34 @@
-export class KeyboardInput {}
+export class KeyboardInput {
+    private keyInputDowned: string
+    private keyInputReleased: string
+    constructor() {
+        this.keyInputDowned = ""
+        this.keyInputReleased = ""
+        window.addEventListener("keydown", this.keyDownEventListener)
+        window.addEventListener("keyup", this.keyUpEventListener)
+    }
+    pressed(key: string) {
+        if (this.keyInputDowned == key) {
+            this.keyInputDowned = ""
+            return true
+        }
+        return false
+    }
+    private keyDownEventListener = (event: KeyboardEvent) => {
+        this.keyInputDowned = event.key
+    }
+
+    released(key: string) {
+        if (this.keyInputReleased == key) {
+            this.keyInputReleased = ""
+            return true
+        }
+        return false
+    }
+
+    private keyUpEventListener = (event: KeyboardEvent) => {
+        this.keyInputReleased = event.key
+    }
+
+    
+}
