a5c935e00e48a1974a795b93f00e81b197d88644
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-28 14:10:26 +0700

    (fix): fix position of land in start scene

--- src/game/constants/FixedPosition.ts
+++ src/game/constants/FixedPosition.ts
@@ -15,7 +15,7 @@ export const TOP_BACKGROUND_POSITION: readonly [number, number] = [0, 0]
 export const BOTTOM_BACKGROUND_POSITION: readonly [number, number] = [0, WINDOW_HEIGHT - 50]
 
 // other components
-export const LAND_POSITION_IN_START_SCENE: readonly [number, number] = [120, 580]
+export const LAND_POSITION_IN_START_SCENE: readonly [number, number] = [120, 650]
 export const GAME_OVER_POSITION: readonly [number, number] = [100, 100]
 export const SCORE_POSITION: readonly [number, number] = [0, 50]
 export const YOUR_SCORE_POSITION: readonly [number, number] = [60, 350]
