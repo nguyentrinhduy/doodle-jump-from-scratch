cef7585fef08d6a2153eef7a07b8963608b5130a
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-05-28 16:24:35 +0700

    (fix): fix wrong mappings between letter and arrow keys

--- src/game-engine/input-handler/KeyboardInput.ts
+++ src/game-engine/input-handler/KeyboardInput.ts
@@ -1,30 +1,39 @@
 export class KeyboardInput {
-    private keyDownInput:  { [key: string]: boolean}
-    private keyUpInput: {[key: string]: boolean}
+    private keyDownInput:  { [key: string]: boolean }
+    private keyUpInput: { [key: string]: boolean }
+    private mapKey: { [key: string]: string}
     constructor() {
         this.keyDownInput = {}
         this.keyUpInput = {}
+        this.mapKey = {}
+        this.mapKey['w'] = 'w'
+        this.mapKey['ArrowUp'] = 'w'
+        this.mapKey['a'] = 'a'
+        this.mapKey['ArrowLeft'] = 'a'
+        this.mapKey['s'] = 's'
+        this.mapKey['ArrowDown'] = 's'
+        this.mapKey['d'] = 'd'
+        this.mapKey['ArrowRight'] = 'd'
         window.addEventListener('keydown', this.keyDownEventListener)
         window.addEventListener('keyup', this.keyUpEventListener)
     }
     keyDown(key: string) {
-        if (!(key in this.keyDownInput)) return false
-        return this.keyDownInput[key]
+        if (!(this.mapKey[key] in this.keyDownInput)) return false
+        return this.keyDownInput[this.mapKey[key]]
     }
 
     keyReleased(key: string) {
-        if (!(key in this.keyUpInput)) return false
-        
-        return this.keyUpInput[key]
+        if (!(this.mapKey[key] in this.keyUpInput)) return false
+        return this.keyUpInput[this.mapKey[key]]
     }
 
     private keyDownEventListener = (event: KeyboardEvent) => {
-        this.keyDownInput[event.key] = true
-        this.keyUpInput[event.key] = false
+        this.keyDownInput[this.mapKey[event.key]] = true
+        this.keyUpInput[this.mapKey[event.key]] = false
     }
 
     private keyUpEventListener = (event: KeyboardEvent) => {
-        this.keyDownInput[event.key] = false
-        this.keyUpInput[event.key] = true
+        this.keyDownInput[this.mapKey[event.key]] = false
+        this.keyUpInput[this.mapKey[event.key]] = true
     }
 }
