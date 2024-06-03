ea9825b9a0829ca406c63aa2121b84db42fadcd7
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-06-03 11:35:43 +0700

    (refactor): remove isClicked

--- src/game-engine/game-objects/Button.ts
+++ src/game-engine/game-objects/Button.ts
@@ -3,7 +3,5 @@ import { ImageGameObject } from './ImageGameObject'
 import { GameObject } from './GameObject'
 
 export class Button extends ImageGameObject {
-    isClicked(positionX: number, positionY: number): boolean {
-        return this.containsPoint([positionX, positionY])
-    }
+    
 }
